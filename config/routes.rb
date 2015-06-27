803e795deb6812d84b072fa60117299d9b140fdc
Darvas Gábor <darvas.gabor@soulware.hu>
2015-06-27 16:32:08 +0200

    fix namespace typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  namespase :api, defaults: {format: :json},
+  namespace :api, defaults: {format: :json},
                   contraints: {subdomain: 'api'},
                   path: '/' do
     scope module: :v1 do
-            
+
     end
 
   end
