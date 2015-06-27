ed344d64831ffcb7a482476b24e668c20fb0c737
Darvas Gábor <darvas.gabor@soulware.hu>
2015-06-27 17:47:54 +0200

    add show action to user controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
 
-  devise_for :users
-  namespace :api, defaults: {format: :json}, contraints: {subdomain: 'api'}, path: '/' do
-    scope module: :v1, contraints: ApiConstraints.new(version: 1, default: true) do
-
+  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users, :only => [:show]
     end
   end
 
