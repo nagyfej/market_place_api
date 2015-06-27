e2f54bb527760df2fc51337c2fc3a17b0dd7f769
Darvas Gábor <darvas.gabor@soulware.hu>
2015-06-27 18:02:37 +0200

    fix routes, add null session

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
 
-  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
+  #namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
+  namespace :api, defaults: {format: :json}, path: '/api' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, :only => [:show]
     end
