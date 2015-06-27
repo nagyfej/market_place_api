2c64c32e67ed0f43ecd4d51c8067610a2f807721
Darvas Gábor <darvas.gabor@soulware.hu>
2015-06-27 21:58:17 +0200

    add auth_token to user and generate token on creation

---
+++ db/migrate/20150627194436_add_authorization_token_to_users.rb
@@ -0,0 +1,6 @@
+class AddAuthorizationTokenToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :auth_token, :string, default: ""
+    add_index :users, :auth_token, unique: true
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,8 +9,10 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:auth_token) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
+  it { should validate_uniqueness_of(:auth_token) }
   it { should validate_confirmation_of(:password) }
   it { should allow_value("test@example.com").for(:email) }
 
@@ -19,4 +21,18 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "#generate_authentication_token!" do
+    it "generates a unique token" do
+      Devise.stub(:friendly_token).and_return("auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eql "auniquetoken123"
+    end
+
+    it "generates another token when a token already has been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eql existing_user.auth_token      
+    end
+
+  end
 end
