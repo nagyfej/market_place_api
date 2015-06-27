82bb6c59d9c4fcd0eb333bdec197f50f4d7bca4f
Darvas Gábor <darvas.gabor@soulware.hu>
2015-06-27 16:29:35 +0200

    add API versioning information to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespase :api, defaults: {format: :json},
                   contraints: {subdomain: 'api'},
                   path: '/' do
+    scope module: :v1 do
+            
+    end
 
   end
 
