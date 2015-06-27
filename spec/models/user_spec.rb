6265bf5028965fe74db911900b28f11f22f9d48b
Darvas Gábor <darvas.gabor@soulware.hu>
2015-06-27 17:19:55 +0200

    install shoulda gem and add more user validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,8 +7,16 @@ describe User do
   subject { @user }
 
   it { should respond_to(:email) }
-  it { should respond_to(:avatar) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value("test@example.com").for(:email) }
+
+  describe "when email is not present" do
+    before { @user.email = ""}
+    it { should_not be_valid }
+  end
 
 end
