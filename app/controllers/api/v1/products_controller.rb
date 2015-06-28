29da024e9e1668812f943a98051053a4f4c7c7df
Darvas Gábor <darvas.gabor@soulware.hu>
2015-06-28 15:15:02 +0200

    add destroy to product

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -27,6 +27,12 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    product = current_user.products.find(params[:id])
+    product.destroy
+    render :nothing => true, status: 204
+  end
+
   private
     def product_params
       params.require(:product).permit(:title, :price, :published)
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json}, path: '/api' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, :only => [:show, :create, :update, :destroy] do
-        resources :products, :only => [:create, :update]        
+        resources :products, :only => [:create, :update, :destroy]        
       end
       resources :sessions, :only => [:create, :destroy]
       resources :products, :only => [:index, :show]
