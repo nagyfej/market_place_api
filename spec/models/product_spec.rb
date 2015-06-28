c9a2144ae958f1dc890d3587f605347529986c8b
Darvas Gábor <darvas.gabor@soulware.hu>
2015-06-28 14:18:29 +0200

    add some validations to products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,6 +8,11 @@ describe Product do
   it { should respond_to(:price) }
   it { should respond_to(:published) }
   it { should respond_to(:user_id) }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :price }
+  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+  it { should validate_presence_of :user_id }
+
 
   it { expect(product.published).to be_falsey }
 end
